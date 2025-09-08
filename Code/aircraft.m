178ac7091c67b37437384d05ea8bd20a3cbb9ece
michaelchen <jr.michael.chen@gmail.com>
2025-09-07 21:44:13 -0400

    Aircraft config file

--- Code/aircraft.m
+++ Code/aircraft.m
@@ -1,7 +1,45 @@
 function ac = aircraft()
-% AIRCRAFT  Creates an aircraft struct containing default parameters
-%    ac = AIRCRAFT()  returns the aircraft struct
+% AIRCRAFT  Creates an aircraft struct containing default parameters.
+%    ac = AIRCRAFT()  returns the aircraft struct.
+
+% All units are metric (m-kg-s)
 
 % Initial sizing parameters
-ac.initial.
+% Altitudes
+ac.initial.h_cruise = 40000 .* 0.3048;              % Cruise altitude (ft to m)
+% Velocities and Mach numbers
+ac.initial.M_cruise = 0.84;                         % Cruise Mach number
+ac.initial.V_cruise = getV(ac.initial.h_cruise, ... % Cruise velocity (m/s)
+                            ac.initial.M_cruise);
+ac.initial.M_loiter = 0.35;                         % Loiter Mach number
+% Aircraft geometry
+ac.initial.AR = 3;                                  % Aspect ratio
+% Engine performance
+ac.initial.TSFC_dry = 0.82 ./ 3600;                 % Dry thrust specific fuel consumption (lb/lb-s to N/N-s)
+ac.initial.TSFC_wet = 1.844 ./ 3600;
+ac.initial.T_mil = 2.*14447 .* 4.44822;             % Military thrust (lb to N)
+ac.initial.T_max = 2.*21496 .* 4.44822;
+% Aircraft weights
+ac.initial.W0 = 60000 .* 4.44822;                   % Takeoff weight (lb to N)
+ac.initial.We = 30000 .* 4.44822;                   % Empty weight (lb to N)
+ac.initial.Wf = 24800 .* 4.44822;                   % Fuel weight (lb to N)
+ac.initial.W_crew = 200 .* 4.44822;                 % Crew weight (lb to N)
+ac.initial.W_pay = 5000 .* 4.44822;                 % Payload weight (lb to N)
+
+% Air-to-air mission parameters
+ac.a2a.R = 1000 .* 1852;                            % Combat radius (nm to m)
+ac.a2a.M_dash = 2.0;                                % Dash Mach number
+ac.a2a.t_combat = 5 .* 60;                          % Combat time (min to s)
+ac.a2a.t_loiter = 20 .* 60;                         % Loiter time (min to s)
+
+% Strike mission parameters
+% Altitudes
+ac.strike.R = 1000 .* 1852;                         % Combat radius (nm to m)
+ac.strike.M_dash = 0.85;                            % Combat dash Mach number
+ac.strike.V_dash = getV(0, ac.strike.M_dash);       % Combat dash velocity (m/s)
+ac.strike.R_combat = 100 .* 1852;                   % Combat dash distance (nm to m)
+ac.strike.t_combat = ac.strike.R_combat ./ ...      % Combat dash time (s)
+                     ac.strike.V_dash;
+ac.strike.t_loiter = 20 .* 60;                      % Loiter time (min to s)
+
 end
