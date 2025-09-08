76dab3974c177b9d89e32c047766021658fe159a
michaelchen <jr.michael.chen@gmail.com>
2025-09-07 21:44:37 -0400

    Bug fix

--- Code/Sizing/Regressions/sub_LDmax_est.m
+++ Code/Sizing/Regressions/sub_LDmax_est.m
@@ -3,6 +3,6 @@ function LDmax = sub_LDmax_est(AR, M)
 % number.
 %   LDmax = SUB_LDMAX_EST(AR, M) estimates L/D max.
 
-load("sub_LD_coeffs.mat", sub_LD_coeffs);
-p = sub_LD_coeffs * [AR, 1];
+load("sub_LD_coeffs.mat", "sub_LD_coeffs");
+p = sub_LD_coeffs * [AR; 1];
 LDmax = polyval(p, M);
