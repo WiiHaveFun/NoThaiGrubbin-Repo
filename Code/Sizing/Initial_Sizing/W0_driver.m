e5e1a8878adb1aebaffd648f420c9b168934eb86
michaelchen <jr.michael.chen@gmail.com>
2025-09-08 23:24:01 -0400

    New regressions

--- Code/Sizing/Initial_Sizing/W0_driver.m
+++ Code/Sizing/Initial_Sizing/W0_driver.m
@@ -4,8 +4,10 @@ ac = aircraft();
 % Wfrac_reg.C = -0.053;
 % Wfrac_reg.A = 2.1650 .* 0.224809.^-0.1425;
 % Wfrac_reg.C = -0.1425;
-Wfrac_reg.A = 0.8902 .* 0.224809.^-0.0528;
-Wfrac_reg.C = -0.0528;
+% Wfrac_reg.A = 0.8902 .* 0.224809.^-0.0528;
+% Wfrac_reg.C = -0.0528;
+Wfrac_reg.A = 0.8570 .* 0.224809.^-0.0557;
+Wfrac_reg.C = -0.0557;
 [ac] = iterate_W0(ac, Wfrac_reg, @a2a_Ffrac);
 
 disp(ac.initial.W0 ./ 4.44822);
