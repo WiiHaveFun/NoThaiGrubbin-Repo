6ba3c6ea1c217fa4e71341bf763a5bc9c0dfa1c6
michaelchen <jr.michael.chen@gmail.com>
2025-09-20 21:29:52 -0400

    Make tropical input

--- Code/Sizing/Constraints/climb_rate.m
+++ Code/Sizing/Constraints/climb_rate.m
@@ -1,4 +1,4 @@
-function TW = climb_rate(WS, Vg, V, h, CD0, K, max, OEI, Neng, Wfrac, Tfrac)
+function TW = climb_rate(WS, Vg, V, h, CD0, K, max, OEI, Neng, tropical, Wfrac, Tfrac)
 % CLIMB_RATE  Calculates the required TWR for a climb rate.
 %   [TW] = CLIMB_RATE(WS, M, h)
 
@@ -11,11 +11,15 @@ WS = WS .* Wfrac;
 TW = (Vg + rho.*V.^3.*CD0 ./ (2.*WS) + 2.*K ./ (rho.*V) .* WS) ./ V;
 % Convert to SLS TWR with correction for 50 F above standard
 % TODO change if constraining climbs above sea level
-TW = TW .* Wfrac ./ Tfrac .* (1./0.8);
+TW = TW .* Wfrac ./ Tfrac;
 if max % max continuous thrust
     TW = TW .* (1./0.94);
 end
 
 if OEI
     TW = TW .* (Neng ./ (Neng - 1));
+end
+
+if tropical
+    TW = TW .* (1./0.8);
 end
